92835682386faa9a686d41976fd1a9e5046e4e0b
Babipoki <laughe@laughe-art.com>
2020-03-05 10:37:56 +0200

    Barebones of the generator

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
+<?php $gen = $_GET["gen"]?>
+
 <head>
     <title>Babipoki's Generators</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <?php include "./header.html" ?>
+    <?php include "./header.html"; ?>
 
-
-    <?php include "./footer.html" ?>
+    
+        <?php if (empty($gen)) {
+            echo "Please pick the generator...";
+        } else {
+            
+            include "./" . $gen . ".html";
+        }
+        ?>
+    
+    
+    <?php include "./footer.html"; ?>
 </body>
