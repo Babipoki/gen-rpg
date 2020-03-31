181b656c5ec94ff47580bba3ed1c64e103692517
Babipoki <laughe@laughe-art.com>
2020-03-31 11:45:12 +0300

    More stuff in art generator

--- art/art.js
+++ art/art.js
@@ -25,7 +25,7 @@ function generateArtIdea(complexity, category) {
         // Series / Fanart
         // Series complexity: 0 - super easy, 1 - easy, 2 - medium, 3 - hard, 4 - super hard (complexity can be impacted by how much i like the series or/and how complex the characters are)
         var allSeries = [
-            ["Pokemon", 1, ["Pikachu", "Scorbunny", "Raboot", "Oshawott", "Toxel"]],
+            ["Pokémon", 1, ["Pikachu", "Scorbunny", "Raboot", "Oshawott", "Toxel", "Psyduck", "Drifloon", "Politoed", "Aipom", "Sableye", "Chimchar", "Pansage", "Panpour", "Sobble", "Pancham", "Dedenne", "Togedemaru", "Drizzile", "Morpeko"]],
             ["Mystery Kids", 2, ["Dipper Pines", "Norman Babcock", "Razputin Aquato", "Wirt", "Greg", "Wirt and Greg", "Danny Phantom", "Invader Zim"]],
             ["League of Legends", 2, ["Gnar", "Teemo", "Kennen", "Nunu", "Heimerdinger"]],
             ["Bloons", 2, ["Dart Monkey", "Boomerang Monkey", "Ninja Monkey"]],
@@ -33,7 +33,9 @@ function generateArtIdea(complexity, category) {
             ["South Park", 3, ["Stan Marsh", "Kyle Broflovski", "Eric Cartman", "Kenny McCormic", "Butters Stotch", "Clyde Donovan", "Craig Tucker"]],
             ["Boku no Hero Academia", 4, ["Izuku Midoriya", "Katsuki Bakugo", "Minoru Mineta", "Shoto Todoroki", "Sen Kaibara", "Yosetsu Awase", "Kosei Tsuburaba", "Nirengeki Shoda", "Yuyu Haya"]],
             ["Inazuma Eleven", 2, ["Endou Mamoru", "Guenji Shuuya", "Kidou Yuuto", "Matsukaze Tenma"]],
-            ["Karakai Jouzu no Takagi-san", 2, ["Nishikata", "Nakai", "Hamaguchi"]]
+            ["Karakai Jouzu no Takagi-san", 2, ["Nishikata", "Nakai", "Hamaguchi"]],
+            ["Mini Ninjas", 3, ["Hiro", "Shun", "Tora"]],
+            ["Wakfu", 3, ["Yugo", "Chibi", "Adamaï", "Az", "Sir Percedal of Sadlygrove", "Flopin"]]
         ];
 
         var selectedSeries = allSeries[MathRInt(0, allSeries.length - 1)];
@@ -42,19 +44,22 @@ function generateArtIdea(complexity, category) {
         }
 
         var selectedCharacter = selectedSeries[2][MathRInt(0, selectedSeries[2].length - 1)];
+        var actions = ["enjoying a balloon", "trying to pop a balloon", "hugging a balloon", "celebrating birthday", "protecting self against COVID-19", "spending time at the beach", "trying to be a ninja", "making a silly face", "being a derp", "eating food", "eating something tasty", "drinking something", "inflating a balloon", "being exhausted", "popping balloons"]
+        var action = MathRInt(0, 2) == 1 ? action = (", " + actions[MathRInt(0, actions.length - 1)] ) : "";
 
-        result = `Draw <b><a href="https://google.com/search?q=${selectedCharacter.replace(" ", "+") + "+" + selectedSeries[0].replace(" ", "+") + "&tbm=isch"}">${selectedCharacter}</a></b> from <b>${selectedSeries[0]}</b> series.`;
+        result = `Draw <b><a href="https://google.com/search?q=${selectedCharacter.replace(" ", "+") + "+" + selectedSeries[0].replace(" ", "+") + "&tbm=isch"}">${selectedCharacter}</a></b> from <b>${selectedSeries[0]}</b> series${action}.`;
     }
     // Study
     if (category == "Study") {
         var allStudies = [
             ["environments", 3, ["a lake", "sea", "forest", "plains"]],
-            ["interiors", 4, ["a kid's Room", "a kitchen", "a bedroom", "a shop"]],
+            ["interiors", 4, ["a kid's room", "a kitchen", "a bedroom", "a shop"]],
             ["urban environment", 3, ["a city", "a town", "a village"]],
             ["anatomy", 2, ["body", "legs", "eyes", "arms", "hands", "feet", "ears", "noses", "mouths", "chests", "hips", "adults", "facial expressions"]],
             ["gestures", 1, ["animals", "humans"]],
             ["color", 2, ["Disney color keys", "Google Street Maps", "game screenshots"]], // Go study ___ of the ___.
-            ["props", 3, ["weapons", "furniture", "toys"]],
+            ["props", 3, ["weapons", "toys", "books", "chests", "balloons", "tents"]],
+            ["furniture", 4, ["beds", "chairs", "fridges", "clocks", "bookshelves", "cupboards"]],
             ["misc studies", 2, ["materials", "game levels", "textures", "clothing"]]
         ];
 
@@ -65,17 +70,18 @@ function generateArtIdea(complexity, category) {
 
         var selectedSubject = selectedStudy[2][MathRInt(0, selectedStudy[2].length - 1)];
 
-        result = `Go study the <b>${selectedStudy[0]}</b> of <b>${selectedSubject}</b>.`
+        result = `Go study <b>${selectedSubject}</b> from <b>${selectedStudy[0]}</b> category.`
 
     }
 
-    if (category == "Design") {
+    if (category == "Character Design") {
     // Design
     var characters = [
         ["boy", 1],
         ["youdel", 2],
         ["lafahl", 3],
-        ["animal", 4]
+        ["animal", 4],
+        ["robot", 4]
     ]
 
     var char = characters[MathRInt(0, characters.length - 1)];
@@ -86,11 +92,11 @@ function generateArtIdea(complexity, category) {
     var animals = ["dog", "cat", "tiger", "kitten", "puppy", "bat", "monkey", "panda", "frog", "turtle", "dinosaur", "parrot", "crocodile", "fennec fox", "furret", "mouse", "wolf", "lizard", "hedgehog", "fox", "rabbit", "bunny"];
     if (char[0] == "animal") char[0] = animals[MathRInt(0, animals.length - 1)] + (MathRInt(0, 2) == 2 ? "(or a creature that looks like it)" : "");
 
-    var personalityTraits = ["confident", "nervous", "scared", "angry", "tired", "careless", "wacky", "derpy", "adventurous"]; // personality or mood
+    var personalityTraits = ["confident", "nervous", "scared", "angry", "tired", "careless", "wacky", "derpy", "adventurous", "creepy"]; // personality or mood
     var personalityTrait = personalityTraits[MathRInt(0, personalityTraits.length - 1)];
-    var traits1 = ["loves balloons", "loves camping", "hates balloons", "likes to bully others", "loves danger", "loves flowers"];
+    var traits1 = ["loves balloons", "loves camping", "hates balloons", "likes to bully others", "loves danger", "loves flowers", "hates outdoors", "likes to draw", "is a poet"];
     var trait1 = traits1[MathRInt(0, traits1.length - 1)];
-    var traits2 = ["is a great fighter", "is a nerd", "is an archer", "always gets in trouble", "has rich parents"]
+    var traits2 = ["is a great fighter", "is a nerd", "is an archer", "always gets in trouble", "has rich parents", "is an orphan", "loves to sing"]
     var trait2 = traits2[MathRInt(0, traits2.length - 1)];
      
     
