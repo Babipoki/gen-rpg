082de92fada05bce7f5dee7bb5970e9d99d5fb77
Babipoki <laughe@laughe-art.com>
2020-09-29 11:34:29 +0300

    Some extra game ideas

--- gameidea/gameidea.js
+++ gameidea/gameidea.js
@@ -20,7 +20,7 @@ function generateGameIdea(timeline) {
     var protagonist = "";
     var races = ["a human", "a youdel", "a lafahl", "an animal"]
     var protagonistRace = races[MathRInt(0, races.length - 1)];
-    var genres = ["An arcade", "An RPG", "A racing", "An action-adventure"];
+    var genres = ["An arcade", "An RPG", "A racing", "An action-adventure", "A shooter", "A real-time strategy", "A turn-based strategy"];
     var genre = genres[MathRInt(0, genres.length - 1)];
     var setting = "";
     var faction = "";
@@ -28,38 +28,38 @@ function generateGameIdea(timeline) {
     var enemy = "";
 
     // Stuff
-    var generalSettings = [];
-    var scifiSettings = ["inside the space station", "at the hostile planet", "on Mars"];
-    var modernSettings = ["inside a police station", "in the crime-infested city", "inside the candy factory"];
-    var abstractSettings = ["inside the Emptyness", "inside the Void", "at the Rainbow Island"];
-    var medievalSettings = ["in the countryside", "at the rival's castle"];
+    var generalSettings = ["at the sea", "on land"];
+    var scifiSettings = ["inside the space station", "at the hostile planet", "on Mars", "at the space hub", "at the space flight school", "in space"];
+    var modernSettings = ["inside a police station", "in the crime-infested city", "inside the candy factory", "at the baseement", "on a stranded island", "at the school", "at the pub", "at the fast food joint", "at the movie studio", "at the robot factory"];
+    var abstractSettings = ["inside the Emptyness", "inside the Void", "at the Rainbow Island", "in Hell", "in the Unknown"];
+    var medievalSettings = ["in the countryside", "at the rival's castle", "at the royal court", "in a foreign village"];
     var fantasySettings = ["on top of the floating island", "on a giant pirate ship", "in alternate Earth"];
 
-    var generalProtagonists = ["kid"];
-    var scifiProtagonists = ["ship captain", "ship janitor"];
-    var modernProtagonists = ["police officer", "office worker"];
-    var abstractProtagonists = ["star collector", "world traveller"];
-    var medievalProtagonists = ["knight", "urchin", "clown"];
-    var fantasyProtagonists = ["sorcerer", "necromancer", "ninja", "assassin"];
+    var generalProtagonists = ["kid", "vigilante"];
+    var scifiProtagonists = ["spaceship captain", "spaceship janitor"];
+    var modernProtagonists = ["police officer", "office worker", "homeless person", "thief", "clown", "balloon vendor", "soldier", "general"];
+    var abstractProtagonists = ["star collector", "world traveler", "time traveler"];
+    var medievalProtagonists = ["knight", "urchin", "clown", "general", "commander"];
+    var fantasyProtagonists = ["sorcerer", "necromancer", "ninja", "assassin", "general", "spy", "knight", "paladin", "barbarian", "bard", "cleric", "monk", "wizard", "rogue", "alchemist", "gunslinger", "oracle", "summoner"];
 
     var generalFactions = ["himself"];
-    var scifiFactions = ["Universal Peace Federation", "Interplanetary Foundation", "Space Pirates"];
+    var scifiFactions = ["Universal Peace Federation", "Interplanetary Foundation", "Space Pirates", "Sanago Cosmetics", "Spacequeens, Inc.", "Trader Corporation"];
     var modernFactions = ["the police", "the game development company"];
     var abstractFactions = ["Star Collector Society", "the Disc Cleaning Company"];
-    var medievalFactions = ["town guards", "merchants"];
-    var fantasyFactions = ["the thieves' guild", "the mage guild"];
+    var medievalFactions = ["town guards", "merchants", "the dukes", "the knights", "the outcasts"];
+    var fantasyFactions = ["the thieves' guild", "the mage guild", "a psychic clan"];
 
     var generalGoals = ["tries to survive"];
     var scifiGoals = ["discovers new alien race", "kills all the aliens", "fends the aliens off"];
     var modernGoals = ["seeks fame", "tries to capture the base", "discovers an ancient artifact"];
     var abstractGoals = ["destroys all the balloons", "collects all stars", "deciphers a secret message"];
     var medievalGoals = ["proves loyalty to the King", "tries to usurp the throne of the King", "fights for survival", "fights for honor"];
-    var fantasyGoals = ["learns a powerful spell", "finds a dangerous scroll"];
+    var fantasyGoals = ["learns a powerful spell", "finds a dangerous scroll", "captures an evil lord", "discovers a floating island"];
 
     var generalEnemies = [];
     var scifiEnemies = ["the insane space pirates", "the rogue government of a planet"];
-    var modernEnemies = ["the criminals", "Kudos Corporation"];
-    var abstractEnemies = ["the bubbles", "the balloons", "the dots", "the triangles"];
+    var modernEnemies = ["the criminals", "Kudos Corporation", "the vandals", "the gangs", "the terrorists"];
+    var abstractEnemies = ["the bubbles", "the balloons", "the dots", "the triangles", "the baddies"];
     var medievalEnemies = ["the thieves", "the cultists"];
     var fantasyEnemies = ["the local witch", "the hog", "the Empire"];
 
